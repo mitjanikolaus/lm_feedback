1b6445c5928427388c2808b66678d1aa2e3053be
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-03-29 12:00:07 +0200

    Saving HF compatible checkpoints

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.cli import LightningCLI
-from transformers import RobertaConfig, RobertaForMaskedLM, AdamW
+from transformers import RobertaConfig, RobertaForMaskedLM
+from torch.optim import AdamW
 import pytorch_lightning as pl
 from data import BabyLMDataModule
 
@@ -23,6 +26,20 @@ class BabyLMModel(pl.LightningModule):
 
         self.model = RobertaForMaskedLM(config=config)
 
+    def save_huggingface_checkpoint(self):
+        """Self checkpoint that is compatible with huggingface"""
+        print("Saving huggingface-compatible checkpoint")
+
+        huggingface_ckpt_dir = os.path.join(self.logger.log_dir, "ckpt_huggingface")
+        os.makedirs(huggingface_ckpt_dir, exist_ok=True)
+
+        self.model.save_pretrained(huggingface_ckpt_dir)
+        tokenizer = self.trainer.datamodule.tokenizer
+        tokenizer.save_pretrained(huggingface_ckpt_dir)
+
+    def on_fit_start(self) -> None:
+        self.save_huggingface_checkpoint()
+
     def training_step(self, batch, batch_idx):
         out = self.model(input_ids=batch.input_ids, attention_mask=batch.attention_mask, labels=batch.labels,
                          token_type_ids=batch.token_type_ids)
@@ -35,6 +52,9 @@ class BabyLMModel(pl.LightningModule):
                          token_type_ids=batch.token_type_ids)
         self.log(f"val_loss", out["loss"], prog_bar=True)
 
+    def on_save_checkpoint(self, checkpoint):
+        self.save_huggingface_checkpoint()
+
     def configure_optimizers(self):
         optimizer = AdamW(params=self.model.parameters(), lr=self.hparams.initial_lr)
         return optimizer
@@ -46,7 +66,7 @@ class BabyLMModel(pl.LightningModule):
 
 
 def cli_main():
-    checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min", save_last=True,
+    checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min", save_last=False,
                                           filename="{epoch:02d}-{val_loss:.2f}")
     early_stop_callback = EarlyStopping(monitor="val_loss", patience=10, verbose=True, mode="min",
                                         min_delta=0.01)
