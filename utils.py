f81c7037adbc387322668ab80a5aa08bd2d89db1
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-07-26 16:51:28 +0200

    update paths for 2024 version

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os.path
 
 DATA_DIR = os.path.expanduser("~/data/babylm_data/")
 
-TRAINING_TRACK_SMALL = "babylm_10M"
-TRAINING_TRACK_DEFAULT = "babylm_100M"
+TRAINING_TRACK_SMALL = "train_10M"
+TRAINING_TRACK_DEFAULT = "train_100M"
 
-PATH_DEV = "babylm_dev"
+PATH_DEV = "dev"
