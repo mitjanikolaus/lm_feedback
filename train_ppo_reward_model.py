4cf5d2a82910d3aa30f75bc2c1aed9093ea121a1
mitja <mitja.nikolaus@posteo.de>
2024-08-10 15:49:08 +0200

    ppo reward model trainer

---
+++ train_ppo_reward_model.py
@@ -0,0 +1,115 @@
+import warnings
+
+import torch
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import pandas as pd
+
+from data import compute_reward_value
+from utils import CHILDES_RL_DATA_FILE
+
+tqdm.pandas()
+
+from transformers import AutoTokenizer, HfArgumentParser, AutoModelForSequenceClassification
+from datasets import Dataset, DatasetDict
+
+from trl import RewardConfig, ModelConfig, \
+    get_quantization_config, get_kbit_device_map, RewardTrainer, get_peft_config
+
+
+TEST_SET_SIZE = 0.1
+SPLIT_RANDOM_STATE = 1
+
+
+def build_reward_model_trainer_datasets(fb_data_path=CHILDES_RL_DATA_FILE):
+    data_fb = pd.read_csv(fb_data_path)
+    data_fb["reward"] = data_fb.apply(compute_reward_value, axis=1)
+    del data_fb["response_is_clarification_request"]
+    del data_fb["response_is_acknowledgement"]
+
+    data_train, data_test = train_test_split(data_fb, test_size=TEST_SET_SIZE, shuffle=True,
+                                            random_state=SPLIT_RANDOM_STATE)
+
+    ds_train = Dataset.from_pandas(data_train)
+    ds_test = Dataset.from_pandas(data_test)
+
+    ds_train.set_format(type="torch")
+    ds_test.set_format(type="torch")
+
+    datasets = DatasetDict({"train": ds_train, "test": ds_test})
+    return datasets
+
+
+def main():
+    parser = HfArgumentParser((RewardConfig, ModelConfig))
+    config, model_config = parser.parse_args_into_dataclasses()
+    config.gradient_checkpointing_kwargs = dict(use_reentrant=False)
+
+    ################
+    # Model & Tokenizer
+    ################
+    torch_dtype = (
+        model_config.torch_dtype
+        if model_config.torch_dtype in ["auto", None]
+        else getattr(torch, model_config.torch_dtype)
+    )
+    quantization_config = get_quantization_config(model_config)
+    model_kwargs = dict(
+        revision=model_config.model_revision,
+        device_map=get_kbit_device_map() if quantization_config is not None else None,
+        quantization_config=quantization_config,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_config.model_name_or_path, trust_remote_code=model_config.trust_remote_code, use_fast=True
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_config.model_name_or_path, num_labels=1, trust_remote_code=model_config.trust_remote_code, **model_kwargs
+    )
+
+    if model_config.lora_task_type != "SEQ_CLS":
+        warnings.warn(
+            "You are using a `task_type` that is different than `SEQ_CLS` for PEFT. This will lead to silent bugs"
+            " Make sure to pass --lora_task_type SEQ_CLS when using this script."
+        )
+
+    ################
+    # Dataset
+    ################
+    raw_datasets = build_reward_model_trainer_datasets()
+
+    def preprocess_function(sample):
+        tokenized = tokenizer(sample["utt_transcript_clean"], truncation=True, max_length=config.max_length)
+        tokenized["reward"] = sample["reward"]
+
+        return tokenized
+
+    # Preprocess the dataset and truncate examples that are longer than args.max_length
+    raw_datasets = raw_datasets.map(
+        preprocess_function,
+        batched=True,
+        num_proc=4
+    )
+    train_dataset = raw_datasets["train"]
+    eval_dataset = raw_datasets["test"]
+
+    ################
+    # Training
+    ################
+    trainer = RewardTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=config,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        peft_config=get_peft_config(model_config),
+    )
+    trainer.train()
+    trainer.save_model(config.output_dir)
+
+    metrics = trainer.evaluate()
+    trainer.log_metrics("eval", metrics)
+    print(metrics)
+
+
+if __name__ == "__main__":
+    main()
