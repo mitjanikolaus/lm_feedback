409348f2aa96fb5976c7cd15875988062fe43c43
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-08-14 16:22:05 +0200

    ppo without query

--- train_ppo.py
+++ train_ppo.py
@@ -25,107 +25,168 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 CKPT_DIR = "ckpts_ppo"
 
 
-class ChildesPPOTrainer(PPOTrainer):
-    def __init__(
-            self,
-            config: Optional[PPOConfig] = None,
-            model: Optional[PreTrainedModelWrapper] = None,
-            ref_model: Optional[PreTrainedModelWrapper] = None,
-            tokenizer: Optional[PreTrainedTokenizerBase] = None,
-            dataset: Optional[Union[torch.utils.data.Dataset, Dataset]] = None,
-            optimizer: Optional[torch.optim.Optimizer] = None,
-            data_collator: Optional[typing.Callable] = None,
-            num_shared_layers: Optional[int] = None,
-            lr_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
-            training_data_collator: Optional[typing.Callable] = None,
-    ):
-        super(ChildesPPOTrainer, self).__init__(config, model, ref_model, tokenizer, dataset, optimizer, data_collator,
-                                                num_shared_layers, lr_scheduler, training_data_collator)
+# class ChildesPPOTrainer(PPOTrainer):
+#     def __init__(
+#             self,
+#             config: Optional[PPOConfig] = None,
+#             model: Optional[PreTrainedModelWrapper] = None,
+#             ref_model: Optional[PreTrainedModelWrapper] = None,
+#             tokenizer: Optional[PreTrainedTokenizerBase] = None,
+#             dataset: Optional[Union[torch.utils.data.Dataset, Dataset]] = None,
+#             optimizer: Optional[torch.optim.Optimizer] = None,
+#             data_collator: Optional[typing.Callable] = None,
+#             num_shared_layers: Optional[int] = None,
+#             lr_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
+#             training_data_collator: Optional[typing.Callable] = None,
+#     ):
+#         super(ChildesPPOTrainer, self).__init__(config, model, ref_model, tokenizer, dataset, optimizer, data_collator,
+#                                                 num_shared_layers, lr_scheduler, training_data_collator)
+#
+#     def generate(
+#         self,
+#         query_tensor: Optional[Union[torch.Tensor, typing.List[torch.Tensor]]] = None,
+#         length_sampler: Optional[typing.Callable] = None,
+#         batch_size: int = 4,
+#         return_prompt: bool = True,
+#         generate_ref_response: bool = False,
+#         **generation_kwargs,
+#     ):
+#         """
+#         Generate response with the model given the query tensor.
+#         call the `generate` method of the model.
+#
+#         Args:
+#             query_tensor (`torch.LongTensor`):
+#                 A tensor of shape (`seq_len`) containing query tokens or a list of tensors of shape (`seq_len`).
+#             length_sampler (`Callable`, *optional*):
+#                 Callable that returns the number of newly generated tokens.
+#             batch_size (`int`, *optional):
+#                 Batch size used for generation, defaults to `4`.
+#             return_prompt (`bool`, *optional*):
+#                 If set to `False` the prompt is not returned but only the newly generated tokens, defaults to `True`.
+#             generate_ref_response (`bool`, *optional*):
+#                 If set to `True` the reference response is also generated, defaults to `False`.
+#             generation_kwargs (dict[str, Any]):
+#                 Keyword arguments for generation.
+#
+#         Returns:
+#             `torch.LongTensor`: A tensor of shape (`batch_size`, `gen_len`) containing response tokens.
+#         """
+#         if generate_ref_response:
+#             ref_model = self.model if self.is_peft_model else self.ref_model
+#         if isinstance(query_tensor, List):
+#             response = self._generate_batched(
+#                 self.model,
+#                 query_tensor,
+#                 length_sampler=length_sampler,
+#                 batch_size=batch_size,
+#                 return_prompt=return_prompt,
+#                 **generation_kwargs,
+#             )
+#             if generate_ref_response:
+#                 ref_response = self._generate_batched(
+#                     ref_model,
+#                     query_tensor,
+#                     length_sampler=length_sampler,
+#                     batch_size=batch_size,
+#                     return_prompt=return_prompt,
+#                     **generation_kwargs,
+#                 )
+#
+#         else:
+#             if query_tensor is None:
+#                 # No query given
+#                 with unwrap_model_for_generation(self.model, self.accelerator) as unwrapped_model:
+#                     response = unwrapped_model.generate(**generation_kwargs)
+#             else:
+#                 if len(query_tensor.shape) == 2:
+#                     raise ValueError(
+#                         "query_tensor must be a tensor of shape (`seq_len`) or a list of tensors of shape (`seq_len`)"
+#                     )
+#
+#                 if length_sampler is not None:
+#                     generation_kwargs["max_new_tokens"] = length_sampler()
+#
+#                 with unwrap_model_for_generation(self.model, self.accelerator) as unwrapped_model:
+#                     response = unwrapped_model.generate(input_ids=query_tensor.unsqueeze(dim=0), **generation_kwargs)
+#
+#             if generate_ref_response:
+#                 with unwrap_model_for_generation(
+#                     ref_model, self.accelerator, is_peft_model=self.is_peft_model
+#                 ) as unwrapped_model:
+#                     ref_response = unwrapped_model.generate(
+#                         input_ids=query_tensor.unsqueeze(dim=0), **generation_kwargs
+#                     )
+#
+#             if not return_prompt and not self.is_encoder_decoder:
+#                 response = response[:, query_tensor.shape[0] :]
+#                 if generate_ref_response:
+#                     ref_response = ref_response[:, query_tensor.shape[0] :]
+#
+#         if generate_ref_response:
+#             return response, ref_response
+#         return response
 
-    def generate(
-        self,
-        query_tensor: Optional[Union[torch.Tensor, typing.List[torch.Tensor]]] = None,
-        length_sampler: Optional[typing.Callable] = None,
-        batch_size: int = 4,
-        return_prompt: bool = True,
-        generate_ref_response: bool = False,
-        **generation_kwargs,
-    ):
-        """
-        Generate response with the model given the query tensor.
-        call the `generate` method of the model.
-
-        Args:
-            query_tensor (`torch.LongTensor`):
-                A tensor of shape (`seq_len`) containing query tokens or a list of tensors of shape (`seq_len`).
-            length_sampler (`Callable`, *optional*):
-                Callable that returns the number of newly generated tokens.
-            batch_size (`int`, *optional):
-                Batch size used for generation, defaults to `4`.
-            return_prompt (`bool`, *optional*):
-                If set to `False` the prompt is not returned but only the newly generated tokens, defaults to `True`.
-            generate_ref_response (`bool`, *optional*):
-                If set to `True` the reference response is also generated, defaults to `False`.
-            generation_kwargs (dict[str, Any]):
-                Keyword arguments for generation.
-
-        Returns:
-            `torch.LongTensor`: A tensor of shape (`batch_size`, `gen_len`) containing response tokens.
-        """
-        if generate_ref_response:
-            ref_model = self.model if self.is_peft_model else self.ref_model
-        if isinstance(query_tensor, List):
-            response = self._generate_batched(
-                self.model,
-                query_tensor,
-                length_sampler=length_sampler,
-                batch_size=batch_size,
-                return_prompt=return_prompt,
-                **generation_kwargs,
-            )
-            if generate_ref_response:
-                ref_response = self._generate_batched(
-                    ref_model,
-                    query_tensor,
-                    length_sampler=length_sampler,
-                    batch_size=batch_size,
-                    return_prompt=return_prompt,
-                    **generation_kwargs,
-                )
-
-        else:
-            if query_tensor is None:
-                # No query given
-                with unwrap_model_for_generation(self.model, self.accelerator) as unwrapped_model:
-                    response = unwrapped_model.generate(**generation_kwargs)
-            else:
-                if len(query_tensor.shape) == 2:
-                    raise ValueError(
-                        "query_tensor must be a tensor of shape (`seq_len`) or a list of tensors of shape (`seq_len`)"
-                    )
-
-                if length_sampler is not None:
-                    generation_kwargs["max_new_tokens"] = length_sampler()
-
-                with unwrap_model_for_generation(self.model, self.accelerator) as unwrapped_model:
-                    response = unwrapped_model.generate(input_ids=query_tensor.unsqueeze(dim=0), **generation_kwargs)
-
-            if generate_ref_response:
-                with unwrap_model_for_generation(
-                    ref_model, self.accelerator, is_peft_model=self.is_peft_model
-                ) as unwrapped_model:
-                    ref_response = unwrapped_model.generate(
-                        input_ids=query_tensor.unsqueeze(dim=0), **generation_kwargs
-                    )
-
-            if not return_prompt and not self.is_encoder_decoder:
-                response = response[:, query_tensor.shape[0] :]
-                if generate_ref_response:
-                    ref_response = ref_response[:, query_tensor.shape[0] :]
-
-        if generate_ref_response:
-            return response, ref_response
-        return response
+    # def _generate_batched(
+    #     self,
+    #     model: PreTrainedModelWrapper,
+    #     query_tensors: List[torch.Tensor],
+    #     length_sampler: Optional[typing.Callable] = None,
+    #     batch_size: int = 4,
+    #     return_prompt: bool = True,
+    #     pad_to_multiple_of: Optional[int] = None,
+    #     remove_padding: bool = True,
+    #     **generation_kwargs,
+    # ):
+    #     outputs = []
+    #
+    #     padding_side_default = self.tokenizer.padding_side
+    #     if not self.is_encoder_decoder:
+    #         self.tokenizer.padding_side = "left"
+    #
+    #     # in case we have fewer examples than bs
+    #     batch_size = min(len(query_tensors), batch_size)
+    #
+    #     for i in range(0, len(query_tensors), batch_size):
+    #         if length_sampler is not None:
+    #             generation_kwargs["max_new_tokens"] = length_sampler()
+    #
+    #         # prevent overflow if query tensors are not even multiple of bs
+    #         end_index = min(len(query_tensors), i + batch_size)
+    #
+    #         batch = query_tensors[i:end_index]
+    #         batch_mask = [torch.ones_like(element) for element in batch]
+    #         inputs = {"input_ids": batch, "attention_mask": batch_mask}
+    #
+    #         padded_inputs = self.tokenizer.pad(
+    #             inputs,
+    #             padding=True,
+    #             max_length=None,
+    #             pad_to_multiple_of=pad_to_multiple_of,
+    #             return_tensors="pt",
+    #         ).to(self.current_device)
+    #
+    #         with unwrap_model_for_generation(model, self.accelerator) as unwrapped_model:
+    #             generations = unwrapped_model.generate(**padded_inputs, **generation_kwargs)
+    #
+    #         for generation, mask in zip(generations, padded_inputs["attention_mask"]):
+    #             if not self.is_encoder_decoder:
+    #                 output = generation[(1 - mask).sum() :]  # remove padding
+    #             else:
+    #                 output = generation
+    #
+    #             if not return_prompt and not self.is_encoder_decoder:
+    #                 output = output[(mask).sum() :]  # remove prompt
+    #
+    #             if remove_padding and self.tokenizer.eos_token_id in output[1:]:
+    #                 pad_mask = output[1:] == self.tokenizer.eos_token_id
+    #                 pad_start = torch.nonzero(pad_mask, as_tuple=False)[0, 0].item() + 1
+    #                 output = output[: pad_start + 1]  # keep the eos token at the end
+    #
+    #             outputs.append(output)
+    #
+    #     self.tokenizer.padding_side = padding_side_default
+    #     return outputs
 
 
 def build_policy_trainer_dataset(tokenizer, query_data_path, min_length=1, max_length=4):
@@ -198,7 +259,7 @@ def main(args):
 
     ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(args.policy_model)
 
-    ppo_trainer = ChildesPPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
+    ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator)
 
     value_model = AutoModelForSequenceClassification.from_pretrained(args.value_model)
     value_model_tokenizer = AutoTokenizer.from_pretrained(args.value_model)
@@ -215,8 +276,20 @@ def main(args):
         "pad_token_id": tokenizer.pad_token_id,
     }
 
+    def eval_babylm_metrics():
+        model.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
+        tokenizer.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
+
+        eval_babylm(model="hf", model_args=f"pretrained={os.path.join(CKPT_DIR, args.exp_name)},add_bos_token=True",
+                    tasks=["zorro", "blimp_filtered"],
+                    ppo_trainer=ppo_trainer, device=ppo_trainer.accelerator.device.index)
+
+
     if args.query_max_length > 0:
-        for epoch, batch in enumerate(tqdm(ppo_trainer.dataloader)):
+        for step, batch in enumerate(tqdm(ppo_trainer.dataloader)):
+            if step % args.eval_freq == 0:
+                eval_babylm_metrics()
+
             query_tensors = batch["input_ids"]
 
             #### Get completion from gpt2
@@ -240,33 +313,33 @@ def main(args):
             stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
             ppo_trainer.log_stats(stats, batch, rewards)
 
-            if epoch % args.eval_freq == 0:
-                model.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
-                tokenizer.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
-
-                eval_babylm(model="hf", model_args=f"pretrained={os.path.join(CKPT_DIR, args.exp_name)}",
-                            tasks=["zorro", "blimp_filtered"],
-                            ppo_trainer=ppo_trainer, device=ppo_trainer.accelerator.device.index)
     else:
         for step in tqdm(range(config.steps)):
+            if step % args.eval_freq == 0:
+                eval_babylm_metrics()
+
             #### Get completion from gpt2
             response_tensors = []
 
             batch = dict()
             query_tensors = []
-            for i in range(args.batch_size):
+
+            # Generate with 4 different lengths
+            assert args.mini_batch_size % 4 == 0
+            generation_batch_size = args.mini_batch_size // 4
+            for i in range(4):
                 gen_len = output_length_sampler()
                 generation_kwargs["max_new_tokens"] = gen_len
-                response = ppo_trainer.generate(**generation_kwargs)
-                response_tensors.append(response.squeeze()[-gen_len:])
-                query_tensors.append(response.squeeze()[:-gen_len])
+                query = [torch.tensor([tokenizer.bos_token_id], device=ppo_trainer.current_device)] * generation_batch_size
+                responses = ppo_trainer.generate(query, **generation_kwargs)
+                response_tensors.extend([resp.squeeze()[-gen_len:] for resp in responses])
+                query_tensors.extend(query)
 
-            batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
+            batch["response"] = [tokenizer.decode(r.squeeze()).strip() for r in response_tensors]
 
             #### Compute reward
             texts = batch["response"]
-            texts_encoded = value_model_tokenizer(texts, padding=True, truncation=True, return_tensors="pt",
-                                                  max_length=output_max_length + 10)
+            texts_encoded = value_model_tokenizer(texts, padding=True, return_tensors="pt")
             value_model_outputs = value_model(**texts_encoded)
             rewards = value_model_outputs.logits.squeeze()
             rewards = [torch.tensor(r.item()) for r in rewards]
@@ -275,14 +348,6 @@ def main(args):
             stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
             batch["query"] = [""] * args.batch_size
             ppo_trainer.log_stats(stats, batch, rewards)
-
-            if step % args.eval_freq == 0:
-                model.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
-                tokenizer.save_pretrained(os.path.join(CKPT_DIR, args.exp_name))
-
-                eval_babylm(model="hf", model_args=f"pretrained={os.path.join(CKPT_DIR, args.exp_name)}",
-                            tasks=["zorro", "blimp_filtered"],
-                            ppo_trainer=ppo_trainer, device=ppo_trainer.accelerator.device.index)
 
 
 def parse_args():
