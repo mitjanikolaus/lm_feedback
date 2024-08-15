de59612d23a5ab10e49980b4127422fb32cbde7f
mitja <mitja.nikolaus@posteo.de>
2024-08-15 21:28:03 +0200

    cf classifier finetuning default args

--- train_cf_classifier.py
+++ train_cf_classifier.py
@@ -236,10 +236,10 @@ def main():
     trainer_config_args = RewardConfig
     reward_config_fields = trainer_config_args.__dataclass_fields__
     reward_config_fields["bf16"].default = True
-    reward_config_fields["per_device_train_batch_size"].default = 128
-    reward_config_fields["per_device_eval_batch_size"].default = 128
-    reward_config_fields["logging_steps"].default = 10
-    reward_config_fields["num_train_epochs"].default = 3
+    reward_config_fields["per_device_train_batch_size"].default = 64
+    reward_config_fields["per_device_eval_batch_size"].default = 64
+    reward_config_fields["logging_steps"].default = 1
+    reward_config_fields["num_train_epochs"].default = 10
     reward_config_fields["learning_rate"].default = 1.41e-5
     reward_config_fields["optim"].default = "adamw_torch"
     reward_config_fields["max_length"].default = 256
