57c9fe732ec5a5f70cf2c239ff998d9995df0fff
mitja <mitja.nikolaus@posteo.de>
2024-09-07 14:27:30 +0200

    align utt lengths

--- create_additional_train_datasets.py
+++ create_additional_train_datasets.py
@@ -1,4 +1,6 @@
 import os
+from collections import Counter
+
 import pandas as pd
 from utils import CHILDES_RL_DATA_FILE, DATA_DIR
 
@@ -10,29 +12,30 @@ if __name__ == '__main__':
 
     convs_cr["transcript_clean"] = convs_cr["utt_transcript_clean"]
     convs_cr = convs_cr[["transcript_clean"]]
-    ref_num_words = 0
+    ref_num_words = Counter()
     for sent in convs_cr.transcript_clean.values:
-        ref_num_words += len(sent.split(" "))
+        length = len(sent.split(" "))
+        if length > 40:
+            length = -1  # catch-all for long sentences
+        ref_num_words.update([length])
 
-    print(f"Number of words: {ref_num_words}")
+    print(f"Number of utterances: {len(ref_num_words)}")
+    print(f"Most common num of words: {ref_num_words.most_common(10)}")
     convs_cr.to_csv(os.path.join(DATA_DIR, "child_cr.csv"), index=False)
 
     convs_non_cr = conversations[~conversations.response_is_clarification_request].copy()
     convs_non_cr = convs_non_cr.sample(frac=1, random_state=1).reset_index(drop=True)   # Shuffle data
-    convs_non_cr["transcript_clean"] = convs_non_cr["utt_transcript_clean"]
-    convs_non_cr = convs_non_cr[["transcript_clean"]]
-    num_words = 0
-    target_index = 0
-    for target_index, row in convs_non_cr.iterrows():
-        num_words += len(row.transcript_clean.split(" "))
-        if num_words >= ref_num_words:
-            break
+    convs_non_cr["num_words"] = convs_non_cr.utt_transcript_clean.apply(lambda x: len(x.split(" ")))
+    convs_non_cr["num_words"] = convs_non_cr.num_words.apply(lambda x: -1 if x > 40 else x)
+    convs_non_cr_filtered = []
+    for num_words, count in ref_num_words.items():
+        convs_non_cr_target_num_words = convs_non_cr[convs_non_cr.num_words == num_words]
+        convs_non_cr_filtered.append(convs_non_cr_target_num_words.sample(count))
+    convs_non_cr_filtered = pd.concat(convs_non_cr_filtered)
+    convs_non_cr_filtered["transcript_clean"] = convs_non_cr_filtered["utt_transcript_clean"]
+    convs_non_cr_filtered = convs_non_cr_filtered[["transcript_clean"]]
 
     print(f"Number of utts with CR response: {len(convs_cr)}")
-    print(f"Number of utts without CR response: {len(convs_non_cr)}")
+    print(f"Number of utts without CR response: {len(convs_non_cr_filtered)}")
 
-    convs_non_cr = convs_non_cr.iloc[:target_index - 1]
-
-    convs_non_cr.to_csv(os.path.join(DATA_DIR, "child_non_cr.csv"), index=False)
-
-
+    convs_non_cr_filtered.to_csv(os.path.join(DATA_DIR, "child_non_cr.csv"), index=False)
